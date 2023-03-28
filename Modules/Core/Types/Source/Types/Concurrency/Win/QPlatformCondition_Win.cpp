dcef2c2068345222475d4c84023484387f8703c5
LAPTOP-07457B91\karth <kbusam@sumo-digital.com>
2023-03-28 17:19:38 +0530

    Removed an unnecessary count variable

--- Modules/Core/Types/Source/Types/Concurrency/Win/QPlatformCondition_Win.cpp
+++ Modules/Core/Types/Source/Types/Concurrency/Win/QPlatformCondition_Win.cpp
@@ -16,27 +16,23 @@ namespace Quaint
     void QPlatformCondition::wait(uint32_t timeout)
     {
         m_criticalSection.enter();
-        if(m_count == 0)
         {
-            ++m_count;
             m_waiting = true;
             PCRITICAL_SECTION handle = static_cast<PCRITICAL_SECTION>(m_criticalSection.getPlatformHandle());
+            //Waits on specified condition and unlocks the Critical section. CS must be locked prior to calling this
             SleepConditionVariableCS(&m_condition, handle, timeout);
             m_criticalSection.leave();
             return;
         }
-        ++m_count;
         m_criticalSection.leave();
     }
 
     void QPlatformCondition::signal()
     {
         m_criticalSection.enter();
-        --m_count;
-        if(m_count == 0)
-        {
-            WakeConditionVariable(&m_condition);
-        }
+        
+        WakeConditionVariable(&m_condition);
+        
         m_criticalSection.leave();
     }
 }
