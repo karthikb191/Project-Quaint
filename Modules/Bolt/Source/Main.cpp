ce0370e2a53542bb458f1ea5a18b04be8fcfb9b0
LAPTOP-07457B91\karth <kbusam@sumo-digital.com>
2023-05-05 19:13:22 +0530

    Added and linked vulkan library

--- Modules/Bolt/Source/Main.cpp
+++ Modules/Bolt/Source/Main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
-//#include "../Include/Bolt.h"
-
+#include "../Include/Bolt.h"
+#include <vulkan/vulkan.h>
 int main()
 {
     std::cout << "Hello Renderer!" << std::endl;
-    //Bolt::BoltRenderer::get()->startEngine();
-    //Bolt::BoltRenderer::get()->shutdown();
+    Bolt::BoltRenderer::get()->startEngine();
+    
+    uint32_t instanceCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &instanceCount, nullptr);
+    std::cout << "VK Instances : " << instanceCount << std::endl;
+    
+    Bolt::BoltRenderer::get()->shutdown();
+    
     return 0;
 }
